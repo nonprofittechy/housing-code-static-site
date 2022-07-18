5f33b62dd1d54e7fa1d35ec9c58eeb1b158c79f0
Amos Decker <amdecker9@gmail.com>
2022-07-18 12:04:26 -0400

    added keys to elements, removed unused dependencies

--- src/CategoryPage.js
+++ src/CategoryPage.js
@@ -1,7 +1,6 @@
-import {Link, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 import {ChecklistContext} from "./App";
 import {useContext, useEffect, useState} from 'react';
-import * as Papa from "papaparse";
 import BackButton from "./components/BackButton";
 import CallToAction from "./components/CallToAction";
 
@@ -35,7 +34,7 @@ const CategoryPage = () => {
 		<h2>Problems with {params.categoryName}</h2>
 		{descriptions.map((description, index) => {
 			const isEven = index % 2 === 0;
-				return <div id={index} className={'section'} style={{backgroundColor: isEven ? '#EEEEEE' : "white"}}>
+				return <div key={index} className={'section'} style={{backgroundColor: isEven ? '#EEEEEE' : "white"}}>
 					<div style={{width: '70%', margin: '0 auto'}}>
 						<h3>{description}</h3>
 						<div style={{textAlign: 'left'}}>{checklist[relevantChecklistIndices[index]][6]}</div>

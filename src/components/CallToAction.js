fb2c15d0cc9842eee5d7582f88f6a7ffdc6e3837
plocket <52798256+plocket@users.noreply.github.com>
2022-12-09 22:00:10 -0500

    Duplicate staging site styles for "action box"
    TODO: 'Get the repairs I need' and other action buttons should be capital case to be consistent
    
    Also cleaned up some css order stuff and footer stuff

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -4,13 +4,16 @@ import {LanguageCodeContext} from "../App";
 const CallToAction = (props) => {
 	const [languageCode, setLanguageCode] = useContext(LanguageCodeContext);
 
-	return <div className={'section'}>
-		<h2>{translations[languageCode]["Find out if your home is safe according to the state sanitary code. Get the repairs that you need."]}</h2>
-		<a className={'link-no-decoration call-to-action-link button'}
-		   href={'https://interviews.gbls.org/start/uptocode'}>
-			{translations[languageCode]['Get the repairs I need']}
-		</a>
-	</div>;
+
+  return <div className={'action-box'}>
+    <div className={'action-body'}>
+      <h2>{translations[languageCode]["Find out if your home is safe according to the state sanitary code. Get the repairs that you need."]}</h2>
+      <a className={'link-no-decoration call-to-action-link button'}
+         href={'https://interviews.gbls.org/start/uptocode'}>
+        {translations[languageCode]['Get the repairs I need']}
+      </a>
+    </div>
+  </div>;
 }
 
 export default CallToAction;

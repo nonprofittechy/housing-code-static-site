76f178a83a49d217e30549f7ffed457ff4415741
Quinten Steenhuis <qsteenhuis@gmail.com>
2023-02-19 21:29:49 -0500

    Updates

--- src/components/UpToCodeLogo.js
+++
@@ -1,8 +0,0 @@
-const UpToCodeLogo = props => {
-	return  <a className={'logo link'} href={"https://getuptocode.org"}>
-		<img src={process.env.PUBLIC_URL + "/logoWithText.svg"} alt="Up to Code logo"/>
-
-	</a>
-}
-
-export default UpToCodeLogo;
